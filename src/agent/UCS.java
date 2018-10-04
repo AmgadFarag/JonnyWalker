7e839512e7c75c2e0b25f594afe17f0e2dafa87f
AmrAhmedAli <amr.ali-hassan@student.guc.edu.eg>
2018-10-04 10:43:00 +0200

    UCS Update

--- src/agent/UCS.java
+++ src/agent/UCS.java
@@ -3,28 +3,29 @@ import java.util.PriorityQueue;
 import agent.structures.Operator;
 import agent.structures.SearchTreeNode;
 import agent.structures.State;
+import models.MiniMap;
 public class UCS extends Search{
     private SearchTreeNode root;
 	private PriorityQueue<SearchTreeNode> queue;
-	public UCS(SearchTreeNode root, WorldHandler newWorld) {
+	public UCS(SearchTreeNode root, MiniMap newWorld) {
 		// TODO Auto-generated constructor stub
-		world = newWorld;
 		this.root = root;
 		queue = new PriorityQueue<SearchTreeNode>();
 		queue.add(root);
+		begin(newWorld);
 	}
-	public SearchTreeNode begin() {
+	public SearchTreeNode begin(MiniMap newWorld) {
 
 		while(true){
 			if(queue.isEmpty()){
 				System.out.println("no more moves -ucs");
-				break;
+				return null;
 			}
 			SearchTreeNode current = queue.poll();
 			System.out.println(current);
 			if (isGoal(current))
 					return current;
-		    SearchTreeNode[] expandednodes = expandNode(current);
+		    SearchTreeNode[] expandednodes = expandNode(newWorld,current);
 			for (int j = expandednodes.length - 1; j >= 0; j--) {
 						queue.add(expandednodes[j]);
 					}
